95aa67d2c10a98ca64d3f2b6c4d60ba8d8ca2779
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-08-03 12:17:26 +0900

    dockerfile for clone

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 export $(xargs < .env)
 if [ -d /home/appuser/devcon/${NAME_FRONT} ]; then
   cd /home/appuser/devcon/${NAME_FRONT}
-  # npm run dev
+  sudo -i -u root bash -c 'cd /home/appuser/devcon/'${NAME_FRONT}' && npm install'
 else
-  # rm -rf /home/appuser/devcon/${NAME_FRONT};
-  npx create-next-app@12.3.4 ${NAME_FRONT} --ts --yes
+  cd /home/appuser/devcon/
+  sudo npx create-next-app@12.3.4 ${NAME_FRONT} --ts --yes  
   cd /home/appuser/devcon/${NAME_FRONT}
-  npm install next@12.3.4 react react-dom typescript @types/react
-  npm install @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons react-hook-form
+  sudo npm install next@12.3.4 react react-dom typescript @types/react
+  sudo npm install @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons react-hook-form
+  sudo chown appuser:appgroup -R /home/appuser/devcon
 fi
 
-npm run dev
+cd /home/appuser/devcon/${NAME_FRONT}
+sudo npm run dev
 # tail -f /dev/null
